2fb885c9377e60585b8fc74730c6600d822c5a36
gwoo <gwoohoo@gmail.com>
2011-10-15 21:51:33 +0900

    fixing index photos

--- views/photos/index.html.php
+++ views/photos/index.html.php
@@ -5,7 +5,7 @@
 
 <?php foreach ($photos as $photo): ?>
 <div class="photo" style="width:120px;text-align:center;float:left">
-	<?=$this->html->image("/photos/view/{$photo->_id}.jpg", array('width'=> 100)); ?>
+	<?=$this->html->image("/photos/view/{$photo->_id}.jpe", array('width'=> 100)); ?>
 	<?=$this->html->link($photo->title, array('Photos::view', 'id' => $photo->_id)); ?>
 </div>
 <?php endforeach ?>
